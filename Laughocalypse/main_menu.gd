72750d97fc21b59a698fd7d927c1d6c52327b495
Marco Mettke <marco.mettke@tradebyte.com>
2024-01-26 20:33:32 +0100

    add main menu

---
+++ Laughocalypse/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+func _on_quit_pressed():
+	get_tree().quit()
