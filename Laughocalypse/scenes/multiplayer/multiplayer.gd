6feaef7eb94f6cb1224f09e48c2006c74b4d6ba6
Dominik Meyer <domeyer@TB-L169.corp.ad.zalando.net>
2024-01-27 07:24:45 +0100

    blabla

--- Laughocalypse/scenes/multiplayer/multiplayer.gd
+++ Laughocalypse/scenes/multiplayer/multiplayer.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 const PORT = 28960
 #const ADDRESS = "49.13.33.38"
 const ADDRESS = "127.0.0.1"
@@ -7,26 +8,57 @@ const MAX_CONNECTIONS = 100
 var game = {
 	"players": {}
 }
+var myId = 0
+var name2 = ""
 
 func _ready():
-	multiplayer.peer_connected.connect(_on_per_connected)
-	multiplayer.connected_to_server.connect(_on_player_connected)
-
 	if DisplayServer.get_name() == "headless":
 		var peer = ENetMultiplayerPeer.new()
 		peer.set_bind_ip(ADDRESS)
 		var error = peer.create_server(PORT, MAX_CONNECTIONS)
-		multiplayer.multiplayer_peer = peer
+		multiplayer.set_multiplayer_peer(peer)
 		print("started the server on port " + str(PORT))
+		name2 = generate_word(characters, 5)
+		multiplayer.peer_connected.connect(_on_peer_connected)
+		print("wtfwtf")
 	else:
 		var peer = ENetMultiplayerPeer.new()
 		var error = peer.create_client(ADDRESS, PORT)
-		multiplayer.multiplayer_peer = peer
-		
-func _on_player_connected():
-	print(multiplayer.get_unique_id())
+		multiplayer.set_multiplayer_peer(peer)
+		myId = peer.get_unique_id()
+		name2 = generate_word(characters, 5)
+		multiplayer.connected_to_server.connect(_on_connected_to_server)
+		print("wtf")
 
-func _on_per_connected(id):
-	game.players[id] = {
-		"name": 123
-	}
+func _on_connected_to_server():
+	print("ja")
+	#SendPlayerInformation.rpc_id(id, name2)
+
+# get called on server
+func _on_peer_connected(id):
+	print(id)
+	#SendPlayerInformation.rpc_id(id, name2)
+
+@rpc("any_peer", "reliable")
+func SendPlayerInformation(name):
+	game.players[multiplayer.get_remote_sender_id()] = name
+
+	print("" + str(multiplayer.get_remote_sender_id()) + " called " + str(myId) + "to add " +  name)
+	var scene = load("res://scenes/player.tscn")
+	var instance = scene.instantiate()
+	if game.players.size() > 1:
+		instance.position.x = 150
+		instance.position.y = 50
+	else:
+		instance.position.x = 100
+		instance.position.y = 50
+	get_node("/root/Main").add_child(instance)
+
+var characters = 'abcdefghijklmnopqrstuvwxyz'
+
+func generate_word(chars, length):
+	var word: String
+	var n_char = len(chars)
+	for i in range(length):
+		word += chars[randi()% n_char]
+	return word
