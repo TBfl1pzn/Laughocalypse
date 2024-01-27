32c77fcc1af3dede66dafa2f3baafbba0f1efcb3
Martin Jainta <martin.jainta@tradebyte.com>
2024-01-27 08:31:19 +0100

    Fix player speed and input handling

--- Laughocalypse/scenes/player.gd
+++ Laughocalypse/scenes/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
+@export var speed = 100 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.	
 var feather_spawn: Marker2D
 @export var feather_scene: PackedScene
@@ -11,32 +11,13 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func handleInput():
-	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var moveDirection = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = moveDirection * speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	look_at(get_global_mouse_position())
-	
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		print("move_right")
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		print("move_left")
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		print("move_down")
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		print("move_up")
-		velocity.y -= 1
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		
-	position += velocity * delta
-	
 	if Input.is_action_pressed("shoot"):
 		if Global.feather_number == 1:
 			Global.feather_number = 0
