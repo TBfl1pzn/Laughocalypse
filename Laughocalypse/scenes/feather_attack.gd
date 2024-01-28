78d466b32cf17a0921baa7d0b927823e749fdf0f
Martin Jainta <martin.jainta@tradebyte.com>
2024-01-28 13:48:46 +0100

    Fix random crash on enemy kill

--- Laughocalypse/scenes/feather_attack.gd
+++ Laughocalypse/scenes/feather_attack.gd
@@ -15,31 +15,8 @@ func _process(delta):
 	if Input.is_action_pressed("hit") and Global.feather_number == 1 and attack_cooldown.is_stopped():
 		attack_cooldown.start()
 		player.attackMelee()
-		for i in enemies_in_area:
-			i.get_parent().hit()
-
-#func _on_body_entered(body):
-	#if body.is_in_group("enemyHitboxes"):
-		#enemies_in_area.append(body)
-#
-#func _on_body_exited(body):
-	#if body.is_in_group("enemyHitboxes"):
-		#var count_up = 0
-		#for i in enemies_in_area:
-			#if i == body:
-				#enemies_in_area.remove_at(count_up)
-				#count_up += 1
-
-
-func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	if area.is_in_group("enemyHitboxes"):
-		enemies_in_area.append(area)
-
-
-func _on_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
-	if area and area.is_in_group("enemyHitboxes"):
-		var count_up = 0
-		for i in enemies_in_area:
-			if i == area:
-				enemies_in_area.remove_at(count_up)
-				count_up += 1
+		var overlap_areas = get_overlapping_areas()
+		for area in overlap_areas:
+			if area.is_in_group("enemyHitboxes"):
+				var enemy = area.get_parent()
+				enemy.hit()
